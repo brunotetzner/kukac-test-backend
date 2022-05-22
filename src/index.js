5342bce737ac647e4c4088dba558188af412c4de
Bruno Tetzner <brunoltetzner@gmail.com>
2022-05-22 09:08:01 -0300

    ajustes para o deploy

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ app.use("/api", palindromesRoutes);
 app.use("/api", salesRoutes);
 app.use("/api", automoveisRoutes);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`App running!\nhttp://localhost:${port}/`));
